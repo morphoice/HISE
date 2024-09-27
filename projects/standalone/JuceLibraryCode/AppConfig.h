e51e1669d8efed98e697f542cb08876ae5a93116
morphoice <morphoice@gmail.com>
2024-09-27 13:15:51 +0200

    Update AppConfig.h
    trigger.c shmup problem since everything was updated to 4.1.3

--- projects/standalone/JuceLibraryCode/AppConfig.h
+++ projects/standalone/JuceLibraryCode/AppConfig.h
@@ -22,30 +22,7 @@
 
 // [END_USER_CODE_SECTION]
 
-/*
-  ==============================================================================
-
-   In accordance with the terms of the JUCE 6 End-Use License Agreement, the
-   JUCE Code in SECTION A cannot be removed, changed or otherwise rendered
-   ineffective unless you have a JUCE Indie or Pro license, or are using JUCE
-   under the GPL v3 license.
-
-   End User License Agreement: www.juce.com/juce-6-licence
-
-  ==============================================================================
-*/
-
-// BEGIN SECTION A
-
-#ifndef JUCE_DISPLAY_SPLASH_SCREEN
- #define JUCE_DISPLAY_SPLASH_SCREEN 0
-#endif
-
-// END SECTION A
-
-#define JUCE_USE_DARK_SPLASH_SCREEN 1
-
-#define JUCE_PROJUCER_VERSION 0x60103
+#define JUCE_PROJUCER_VERSION 0x80001
 
 //==============================================================================
 #define JUCE_MODULE_AVAILABLE_hi_backend                  1
@@ -88,6 +65,10 @@
 
 #ifndef    USE_WORKBENCH_EDITOR
  //#define USE_WORKBENCH_EDITOR 0
+#endif
+
+#ifndef    HISE_PAINT_GLOBAL_MOD_CONNECTIONS
+ //#define HISE_PAINT_GLOBAL_MOD_CONNECTIONS 0
 #endif
 
 //==============================================================================
@@ -402,6 +383,10 @@
 
 #ifndef    HISE_SAMPLER_CUBIC_INTERPOLATION
  //#define HISE_SAMPLER_CUBIC_INTERPOLATION 0
+#endif
+
+#ifndef    HISE_SAMPLER_ALLOW_RELEASE_START
+ //#define HISE_SAMPLER_ALLOW_RELEASE_START 1
 #endif
 
 //==============================================================================
